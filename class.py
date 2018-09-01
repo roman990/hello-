87329b9e9afba09ce45386cd8a8e89d1cc33ef6e
roman990 <42899435+roman990@users.noreply.github.com>
2018-09-02 03:14:54 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,18 @@
+class enemy:
+    def nameing(self):
+         print('chirag')
+
+class pick:
+    def nameed(self):
+        print('player')
+
+
+class base(enemy,pick):
+     def name(self):
+         print('named')
+
+
+
+c=base()
+c.nameing()
+    
